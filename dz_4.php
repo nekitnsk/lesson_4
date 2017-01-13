cd2bab3150bf20137479990c702e69a42714a7d6
EugenyEmelin <emelin.eug2@ya.ru>
2017-01-13 03:37:01 +0300

    fix structure#2

--- dz_4.php
+++ dz_4.php
@@ -7,7 +7,6 @@
 		td:last-child {font-weight: bold; }
 		h2 {font: 2em helvetica;}
 		i {font-size: .75em; }
-		div {float: right; width: 30%; border: 1px solid grey; }
 		span {color: red; font:.7em/1 arial;}
 		tfoot td{text-align: right; border: none; line-height: 1.4}
 	</style>
@@ -36,8 +35,6 @@ foreach ($bd as $product => $specification) {
 	 $product;
 	foreach ($specification as $key => $value) 
 		 $key.':' .$value;
-		 
-
 		//создадим и заполним индексные массивы данными о товарах
 	$prod[] = $product; #массив названий заказанных товаров
 	$cost[] = $specification['цена']; #массив цен всех товаров
@@ -65,17 +62,8 @@ for ($i = 0; $i < count($prod); $i++) {
 }
 	//функция рассчёта суммарной стоимости товаров
 function sumCost($i) {
-	global $cost;
-	global $discounted_cost;
-	global $quantity;
-	global $balance;
-	global $discount;
-	global $N;
-	global $D;
-	global $ind_discount;
-	global $vol_order_disc;
-	global $summa;
-	global $not_available;
+	global $cost,$discounted_cost,$quantity,$balance,$discount,
+		   $N,$D,$ind_discount,$vol_order_disc,$summa,$not_available;
 	
 	$ind_discount[$i] = $discount[$i]; #добавим индивидуальную скидку в соответствующий массив 
 	if ($quantity[$i]>$balance[$i]) { #если заказано больше, чем имеется наличии
@@ -95,15 +83,9 @@ function sumCost($i) {
 echo'
 <table>
 	<tr>
-		<th>ПРОДУКТ</th>
-		<th>ЦЕНА</th>
-		<th>СКИДКА</th>
-		<th>СКИДКА ОТ ОБЪЁМА</th>
-		<th>СУММАРНАЯ СКИДКА</th>
-		<th>СТОИМОСТЬ СО СКИДКОЙ</th>
-		<th>КОЛИЧЕСТВО</th>
-		<th>В НАЛИЧИИ</th>
-		<th>СУММА</th>
+		<th>ПРОДУКТ</th> <th>ЦЕНА</th> <th>СКИДКА</th> <th>СКИДКА ОТ ОБЪЁМА</th>
+		<th>СУММАРНАЯ СКИДКА</th> <th>СТОИМОСТЬ СО СКИДКОЙ</th> <th>КОЛИЧЕСТВО</th>
+		<th>В НАЛИЧИИ</th> <th>СУММА</th>
 	</tr>';
 	for ($i = 0; $i < count($prod); $i++) {
 		echo'<tr>
@@ -113,20 +95,20 @@ echo'
 			   <td>'.$vol_order_disc[$i].' <i>руб.</i></td>
 			   <td>'.$discount[$i].' <i>руб.</i></td>
 			   <td>'.$discounted_cost[$i].' <i>руб.</i></td>
-			   <td>'.$quantity[$i].' <i>шт.</i><br><span>'.$not_available[$i].'</span></i></td>
+			   <td>'.$quantity[$i].' <i>шт.</i><br><span>'.$not_available[$i].'</span></td>
 			   <td>'.$balance[$i].' <i>шт.</i></td>
 			   <td>'.$summa[$i].' <i>руб.</i></td>
 			 <tr>';
 	}
 	echo '
 	<tfoot>
-		<tr>
-			<td colspan="8"><br>Итого:<br>
-					Сумма заказа: '.array_sum($summa).' руб.<br>
-					Всего наименований: '.count($prod).'<br>
-					Заказано товаров: '.array_sum($quantity).' шт.<br>
-			</td>
-		</tr>
+	  <tr>
+		<td colspan="8"><br>Итого:<br>
+			Сумма заказа: '.array_sum($summa).' руб.<br>
+			Всего наименований: '.count($prod).'<br>
+			Заказано товаров: '.array_sum($quantity).' шт.<br>
+		</td>
+	  </tr>
 	</tfoot>
 </table>';
 ?>
